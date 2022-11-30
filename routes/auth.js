9f1b5235de08acec06810ff12259af3c4db58211
Victor Alejandro Rojas Yovaniniz <alejandro.yovaniniz@gmail.com>
2022-11-30 17:03:57 -0300

    cambiando puerto

--- routes/auth.js
+++ routes/auth.js
@@ -20,7 +20,6 @@ const { validateJWT } = require("../validator/validar-jwt");
 router.post(
   "/register",
   [
-    //middlewares
     check("name", "El nombre es obligatorio").not().isEmpty(),
     check("email", "El email es obligatorio").isEmail(),
     check(
